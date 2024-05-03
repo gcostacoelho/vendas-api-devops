74348dd8d1d7c821e9bbcec3f6f484ebbf390c47
Rezende-Fabio <rezendefabio337@gmail.com>
2024-05-02 23:49:33 -0300

    create user (POST)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
 
-    @Get()
-    async getAllUsers(){
-        return JSON.stringify({teste: "123"})
+    @Post()
+    async createUser(
+        @Body() createUser: CreateUserDto
+    ) {
+        return {
+            ...createUser,
+            password: undefined
+        };
     }
 }
