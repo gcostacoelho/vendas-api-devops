5384bc9a010b99f64ae61ac77000b38d4b2215b1
Rezende-Fabio <rezendefabio337@gmail.com>
2024-05-10 13:51:20 -0300

    FEAT: added auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -30,6 +30,20 @@ export class UserService {
         return user;
     }
 
+    async getUserByEmail(userEmail: string): Promise<UserEntity>{
+        const user = await this.userRepository.findOne({
+            where: {
+                email: userEmail,
+            }
+        });
+
+        if (!user){
+            throw new NotFoundException(`UserEmail: ${userEmail} not found`);
+        }
+
+        return user;
+    }
+
     async getUserByIdUsingRelations(userId: number): Promise<UserEntity>{
         return this.userRepository.findOne({
             where: {
