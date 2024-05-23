e0743c57fa41545b647b2ea54938c725b46440fa
Rezende-Fabio <rezendefabio337@gmail.com>
2024-05-23 12:58:19 -0300

    FEAT: create unit test in stateService

---
+++ src/state/__mocks__/state.mock.ts
@@ -0,0 +1,9 @@
+import { StateEntity } from "../entities/state.entity";
+
+export const StateMock: StateEntity = {
+    createdAt: new Date(),
+    id: 1522,
+    uf: "SP",
+    name: "nameMock",
+    updatedAt: new Date(),
+}
