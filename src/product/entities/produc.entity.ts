cf5fbf5b8984536066a7887bc47185ca8fa99796
Rezende-Fabio <rezendefabio337@gmail.com>
2024-07-23 20:17:11 -0300

    FEAT: module, controller, service and product entity added

---
+++ src/product/entities/produc.entity.ts
@@ -0,0 +1,30 @@
+import { CategoryEntity } from "../../category/entities/categoty.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'product'})
+export class ProductEntity {
+    @PrimaryGeneratedColumn('rowid')
+    id: number;
+
+    @Column({name: 'name', nullable: false})
+    name: string;
+
+    @Column({name: 'category_id', nullable: false})
+    categoryId: number;
+
+    @Column({name: 'price', nullable: false})
+    price: number;
+
+    @Column({name: 'image', nullable: false})
+    image: string;
+
+    @Column({name: 'created_at'})
+    createdAt: Date;
+
+    @Column({name: 'updated_at'})
+    updatedAt: Date;
+
+    @ManyToOne(() => CategoryEntity, (category) => category.products)
+    @JoinColumn({name: 'category_id', referencedColumnName: 'id'})
+    categoty?: CategoryEntity;
+}
