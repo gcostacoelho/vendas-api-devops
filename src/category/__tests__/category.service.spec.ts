75f8d592f5291ecd63a6cb6566ef996c355a8afe
Rezende-Fabio <rezendefabio337@gmail.com>
2024-07-23 21:27:45 -0300

    FEAT: insert unit test on CategoryService in function getAllCategories

--- src/category/__tests__/category.service.spec.ts
+++ src/category/__tests__/category.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from '../category.service';
+import { Repository } from 'typeorm';
+import { CategoryEntity } from '../entities/categoty.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { categoryMock } from '../__mocks__/category.mock';
+
 
 describe('CategoryService', () => {
   let service: CategoryService;
+  let categoryRepository: Repository<CategoryEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: getRepositoryToken(CategoryEntity),
+          useValue: {
+            find: jest.fn().mockResolvedValue([categoryMock]),
+            save: jest.fn().mockResolvedValue(categoryMock),
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
+    categoryRepository = module.get<Repository<CategoryEntity>>(getRepositoryToken(CategoryEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(categoryRepository).toBeDefined();
   });
+
+  it('should return list of category', async () => {
+    const categories = await service.getAllCategories();
+
+    expect(categories).toEqual([categoryMock]);
+  });
+
+  it('should return erro in list category empty', async () => {
+    jest.spyOn(categoryRepository, "find").mockResolvedValueOnce([]);
+    
+    expect(service.getAllCategories()).rejects.toThrow();
+  });
+
+  it('should return erro in list category exception', async () => {
+    jest.spyOn(categoryRepository, "find").mockRejectedValueOnce(new Error());
+    
+    expect(service.getAllCategories()).rejects.toThrow();
+  });
+
 });
