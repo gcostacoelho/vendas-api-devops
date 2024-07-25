72813550d6999e1fbd5135453df76d0d77a86b45
Rezende-Fabio <rezendefabio337@gmail.com>
2024-07-24 21:24:00 -0300

    FEAT: added try catch in UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadGatewayException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadGatewayException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
@@ -10,71 +10,96 @@ export class UserService {
     constructor(
         @InjectRepository(UserEntity)
         private readonly userRepository: Repository<UserEntity>,
-    ){}
+    ) { }
 
-    async getAllUsers(): Promise<UserEntity[]>{
-        return this.userRepository.find();
-    }
-
-    async getUserById(userId: number): Promise<UserEntity>{
-        const user = await this.userRepository.findOne({
-            where: {
-                id: userId,
-            }
-        });
-
-        if (!user){
-            throw new NotFoundException(`UserId: ${userId} not found`);
+    async getAllUsers(): Promise<UserEntity[]> {
+        try {
+            return this.userRepository.find();
+        } catch (error) {
+            throw new InternalServerErrorException(error);
         }
 
-        return user;
     }
 
-    async getUserByEmail(userEmail: string): Promise<UserEntity>{
-        const user = await this.userRepository.findOne({
-            where: {
-                email: userEmail,
-            }
-        });
-
-        if (!user){
-            throw new NotFoundException(`UserEmail: ${userEmail} not found`);
-        }
-
-        return user;
-    }
-
-    async getUserByIdUsingRelations(userId: number): Promise<UserEntity>{
-        return this.userRepository.findOne({
-            where: {
-                id: userId,
-            },
-            relations: {
-                addresses: {
-                    city: {
-                        state: true
-                    }
+    async getUserById(userId: number): Promise<UserEntity> {
+        try {
+            const user = await this.userRepository.findOne({
+                where: {
+                    id: userId,
                 }
-            },
-        });
-    }
+            });
 
-    async createUser(createUserDto: CreateUserDto): Promise<UserEntity>{
-        const userExist = await this.getUserByEmail(createUserDto.email).catch(() => undefined);
+            if (!user) {
+                throw new NotFoundException(`UserId: ${userId} not found`);
+            }
 
-        if (userExist){
-            throw new ConflictException('E-mail registered in system');
+            return user;
+        } catch (error) {
+            throw new InternalServerErrorException(error);
         }
 
-        const saltOrRounds = 10;
-        const passwordHashed = await hash(createUserDto.password, saltOrRounds); 
+    }
 
-        const user = await this.userRepository.save({
-            ...createUserDto,
-            typeUser: 1,
-            password: passwordHashed
-        });
-        
-        return user;
+    async getUserByEmail(userEmail: string): Promise<UserEntity> {
+        try {
+            const user = await this.userRepository.findOne({
+                where: {
+                    email: userEmail,
+                }
+            });
+
+            if (!user) {
+                throw new NotFoundException(`UserEmail: ${userEmail} not found`);
+            }
+
+            return user;
+        } catch (error) {
+            throw new InternalServerErrorException(error);
+        }
+
+    }
+
+    async getUserByIdUsingRelations(userId: number): Promise<UserEntity> {
+        try {
+            return this.userRepository.findOne({
+                where: {
+                    id: userId,
+                },
+                relations: {
+                    addresses: {
+                        city: {
+                            state: true
+                        }
+                    }
+                },
+            });
+        } catch (error) {
+            throw new InternalServerErrorException(error);
+        }
+
+    }
+
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+        try {
+            const userExist = await this.getUserByEmail(createUserDto.email).catch(() => undefined);
+
+            if (userExist) {
+                throw new ConflictException('E-mail registered in system');
+            }
+
+            const saltOrRounds = 10;
+            const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+
+            const user = await this.userRepository.save({
+                ...createUserDto,
+                typeUser: 1,
+                password: passwordHashed
+            });
+
+            return user;
+        } catch (error) {
+            throw new InternalServerErrorException(error);
+        }
+
     }
 }
