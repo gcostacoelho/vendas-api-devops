d65bd3e11356222dfc6bede6fcdf502603613938
Rezende-Fabio <rezendefabio337@gmail.com>
2024-07-24 21:15:14 -0300

    FEAT: added try catch in AddressService

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { AddressEntity } from './entities/address.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,35 +14,44 @@ export class AddressService {
         private readonly addressRepository: Repository<AddressEntity>,
         private readonly userService: UserService,
         private readonly cityService: CityService,
-    ){}
+    ) { }
 
-    async createAddress(createAddressDto: CreateAddressDto, userId: number): Promise<AddressEntity>{
-        await this.userService.getUserById(userId);
-        await this.cityService.getCityById(createAddressDto.cityId);
-        
-        return this.addressRepository.save({
-            ...createAddressDto,
-            userId,
-        });
-    }
+    async createAddress(createAddressDto: CreateAddressDto, userId: number): Promise<AddressEntity> {
+        try {
+            await this.userService.getUserById(userId);
+            await this.cityService.getCityById(createAddressDto.cityId);
 
-    async getAddressByUserId(userId: number): Promise<AddressEntity[]>{
-        const addresses = await this.addressRepository.find({
-            where: {
+            return this.addressRepository.save({
+                ...createAddressDto,
                 userId,
-            },
-            relations: {
-                city: {
-                    state: true,
-                }
-            }
-        });
-
-        if (!addresses || addresses.length === 0){
-            throw new NotFoundException(`Address not found for userId ${userId}`)
+            });
+        } catch (error) {
+            throw new InternalServerErrorException(error)
         }
 
-        return addresses;
+    }
+
+    async getAddressByUserId(userId: number): Promise<AddressEntity[]> {
+        try {
+            const addresses = await this.addressRepository.find({
+                where: {
+                    userId,
+                },
+                relations: {
+                    city: {
+                        state: true,
+                    }
+                }
+            });
+
+            if (!addresses || addresses.length === 0) {
+                throw new NotFoundException(`Address not found for userId ${userId}`)
+            }
+
+            return addresses;
+        }catch (error) {
+            throw new InternalServerErrorException(error)
+        }
     }
 
 }
