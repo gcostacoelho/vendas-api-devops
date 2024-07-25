b39bad80e4ab8648c2d493895885dc741a593c3e
Rezende-Fabio <rezendefabio337@gmail.com>
2024-07-24 21:17:35 -0300

    FEAT: added try catch in CacheService

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,24 +1,29 @@
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
 export class CacheService {
     constructor(
         @Inject(CACHE_MANAGER) private cacheManager: Cache
-    ){}
+    ) { }
 
-    async getCache<T>(key: string, functionRequest: () => Promise<T>): Promise<T>{
-        const allData: T = await this.cacheManager.get(key);
+    async getCache<T>(key: string, functionRequest: () => Promise<T>): Promise<T> {
+        try {
+            const allData: T = await this.cacheManager.get(key);
 
-        if (allData){
-            return allData;
-        }else{
-            const data: T = await functionRequest();
-            
-            this.cacheManager.set(key, data);   
-            
-            return data;
+            if (allData) {
+                return allData;
+            } else {
+                const data: T = await functionRequest();
+
+                this.cacheManager.set(key, data);
+
+                return data;
+            }
+        } catch (error) {
+            throw new InternalServerErrorException(error);
         }
-        
+
+
     }
 }
